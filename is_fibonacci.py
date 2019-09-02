54e92f289e5e6cb77fd68f7e657a5194d06a0d0f
Dipan Patel <dipapate@cisco.com>
2019-09-03 00:08:11 +0530

    update

--- is_fibonacci.py
+++ is_fibonacci.py
@@ -4,7 +4,9 @@ def fibo(n):
     while a <= n:
         l.append(a)
         a,b = a+b,a
-    if n in l:
-        return n
+    return l
 
-print(fibo(14))
+n = int(input("Enter number n >> "))
+fibo_list=fibo(n)
+if n in fibo_list:
+    print("it is a fibonacci number")
