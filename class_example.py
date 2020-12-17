8bfec5058b0f9f480e3f0459ae4665024224caa8
Dipan Patel <dipan.patel@fungible.com>
2020-12-18 00:54:15 +0530

    update the class

--- class_example.py
+++ class_example.py
@@ -1,9 +1,12 @@
-class A:
+class Parent:
+    pass
+
+class Child(Parent):
     def __init__(self,name):
         self.name = name
 
     def display(self):
         print("My name is: "+self.name)
 
-a=A('dipan')
+a=Child('Divyanshu')
 a.display()
