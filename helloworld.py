a65f9cb62907b8b6b7ffd81695b40dee3f7a7060
Dipan Patel <dipan.patel@fungible.com>
2021-03-11 22:18:51 +0530

    first commit

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,11 @@
 print("This is my first Python Program - hello world!!!")
 print("Welcome to Python Programming")
 print("Welcome Dipan")
-a=25
+a=1000
 print(a)
+b=2000
+
+def fun():
+   pass
+
+fun()
